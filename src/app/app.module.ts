cdf8fcc056203da0250c2a1c711f1b8a80a03be6
Háider Cantillo Colón <cantillohaider10@gmail.com>
2023-11-02 22:32:24 -0500

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { AppComponent } from './app.component';
 import { PracticaComponent } from './Vistas/practica/practica.component';
 import { PruebaGitComponent } from './Vistas/prueba-git/prueba-git.component';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SumarComponent } from './Vistas/sumar/sumar.component';
+import { RestarComponent } from './Vistas/restar/restar.component';
+import { ContadorComponent } from './Vistas/contador/contador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PracticaComponent,
-    PruebaGitComponent
+    PruebaGitComponent,
+    SumarComponent,
+    RestarComponent,
+    ContadorComponent,
   ],
   imports: [
     BrowserModule,
     ButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
